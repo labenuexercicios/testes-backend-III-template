2a1f2d63cb6ddad09a543928c7527706258d0e50
yuzo <yuzokamoto@gmail.com>
2023-05-09 09:20:53 -0300

    update

--- src/errors/BadRequestError.ts
+++
@@ -1,9 +0,0 @@
-import { BaseError } from "./BaseError";
-
-export class BadRequestError extends BaseError {
-    constructor(
-        message: string = "Requisição inválida" // mensagem de erro padrão caso não seja enviado um argumento
-    ) {
-        super(400, message)
-    }
-}
--- src/errors/NotFoundError.ts
+++
@@ -1,9 +0,0 @@
-import { BaseError } from "./BaseError";
-
-export class NotFoundError extends BaseError {
-    constructor(
-        message: string = "Recurso não encontrado" // mensagem de erro padrão caso não seja enviado um argumento
-    ) {
-        super(404, message)
-    }
-}
